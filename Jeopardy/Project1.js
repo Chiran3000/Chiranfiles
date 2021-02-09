c3af4824a42805a9f1b47ae5108ca96704c424c5
Chiran Bhattarai <chiransfiles@Chirans-MBP.fios-router.home>
2021-02-09 14:50:33 -0500

    updated with objects

--- Jeopardy/Project1.js
+++ Jeopardy/Project1.js
@@ -3,9 +3,36 @@ console.log('hello');
 let hone = {
   Jeopardyhone: 'Hyper Text Markup Language',
   firstquestion: 'What is HTML?',
-  secondquestion: 'What is What?',
-  thirdquestion: 'Who?'
+  secondquestion: 'What is Tag?',
+  thirdquestion: 'What is ordered list?'
   }
+
+  let htwo = {
+    Jeopardyhtwo: 'allows contents to placed in between',
+    firstquestion:'what are the tags?',
+    secondquestion: 'What is div?',
+    thirdquestion: 'what is syntax?'
+}
+let hthree = {
+  Jeopardyhthree: 'let you link many different web pages using a single image',
+  firstquestion: 'What is an image map?',
+  secondquestion: 'What is link tag?',
+  thirdquestion:'what is header?'
+}
+
+let hfour = {
+  Jeopardyhfour:'enables you to build consistent, transportable and well-defined style templates',
+  firstquestion: 'what are style sheets?',
+  secondquestion: 'what is the advantage of collapsing white space?',
+  thirdquestion: 'what are applets?'
+}
+
+let hfive = {
+  Jeopardyhfive:'allow you to put a scholling text in a web pages',
+  firstquestion: 'What is a marquee?',
+  secondquestion: 'what are applets?',
+  thirdquestion: 'What is the use of alternative text in image mapping?'
+}
 
   
 
