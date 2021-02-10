3ec6e1219cfce67d79a8d81e38f41bea2e1b9ca7
Chiran Bhattarai <chiransfiles@Chirans-MBP.fios-router.home>
2021-02-10 16:21:52 -0500

    able to add text in div

--- Jeopardy/Project1.js
+++ Jeopardy/Project1.js
@@ -1,38 +1,41 @@
 
 
 
-let hone = {
-  answer : 'Hyper Text Markup Language',
-  question: 'What is HTML?',
-  question2 : 'What is Tag?',
-  question3 : 'What is ordered list?'
-  }
-let htwo = {
-    answer: 'allows contents to placed in between',
-    question:'what are the tags?',
+const questions = {
+  hone: {
+    answer1:'Hyper Text Markup Languange',
+    question1: ['What is HTML?', 'what is tag?', 'what is ordered list?']
+    question2 : 'What is Tag?',
+    question3 : 'What is ordered list?'
+  },
+
+htwo: {
+    answer1: 'allows contents to placed in between',
+    question1:'what are the tags?',
     question2: 'What is div?',
     question3: 'what is syntax?'
-}
-let hthree = {
-  answer: 'let you link many different web pages using a single image',
-  question: 'What is an image map?',
+},
+
+hthree:  {
+  answer1: 'let you link many different web pages using a single image',
+  question1: 'What is an image map?',
   question2: 'What is link tag?',
   question3:'what is header?'
-}
-let hfour = {
-  answer:'enables you to build consistent, transportable and well-defined style templates',
-  question: 'what are style sheets?',
+},
+hfour:  {
+  answer1:'enables you to build consistent, transportable and well-defined style templates',
+  question1: 'what are style sheets?',
   question2: 'what is the advantage of collapsing white space?',
   question3: 'what are applets?'
+},
+ hfive: {
+  answer1:'allow you to put a scroolling text in a web pages',
+  question1: 'What is a marquee?',
+  question2: 'what are applets?',
+  question3: 'What is the use of alternative text in image mapping?'
 }
-let hfive = {
-  answer:'allow you to put a scroolling text in a web pages',
-  question: 'What is a marquee?',
-  question1: 'what are applets?',
-  question2: 'What is the use of alternative text in image mapping?'
 }
-
-const answers = [hone, htwo, hthree, hfour, hfive]
+//const answers = [hone, htwo, hthree, hfour, hfive]
 
 
   
@@ -41,14 +44,14 @@ const answers = [hone, htwo, hthree, hfour, hfive]
 $(() => {   
 
 $('.boxes').on('click', (e) => {
-console.log(e.currentTarget.id)
+//console.log(e.currentTarget.id)
 let obj = e.currentTarget.id
-console.log(obj)
-$('#answers').text(hone)
-console.log(eval(obj))
-    
-    
-  })
+//$('#answers').eval(hone)
+$('#answers').text(eval(questions[obj]).answer1)
+
+
+}
+)
 })
 
  
