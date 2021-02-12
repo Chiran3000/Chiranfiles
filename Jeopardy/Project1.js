ac7a5fddfb8c1af1a0e226bb1871a013226ffffc
Chiran Bhattarai <chiransfiles@Chirans-MBP.fios-router.home>
2021-02-12 08:03:06 -0500

    Almost Finish

--- Jeopardy/Project1.js
+++ Jeopardy/Project1.js
@@ -1,10 +1,16 @@
-let button = document.createElement("button");
-button.innerHTML = "Submit Your Answer Here";
-let body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
-button.addEventListener ("click", function() {
-  alert("That is Correct!!");
-});
+
+/*
+document.addEventListener('keydown', (event) => {
+  if (event.key == '1') {
+    alert('That is Correct!')
+  } else  
+  alert('that is incorrect!')
+
+  });
+
+  
+*/
+
 
 
 
@@ -16,36 +22,52 @@ const questions = {
   },
 
 htwo: {
-    answer1: 'allows contents to placed in between',
-    question1:'what are the tags?',
-    question2: 'What is div?',
-    question3: 'what is syntax?'
+    answer1: 'Allows Contents to Placed in Between',
+    question1:['[1].what are the tags?' + '[2].What is Div' + '[3].What is syntax?']
+    
 },
 
 hthree:  {
-  answer1: 'let you link many different web pages using a single image',
-  question1: 'What is an image map?',
-  question2: 'What is link tag?',
-  question3:'what is header?'
+  answer1: 'Let you link many different web pages using a single image',
+  question1: ['[1].What is an image map?'+ '[2].What is link tag?' + '[3].What is Header?']
+  
 },
 hfour:  {
-  answer1:'enables you to build consistent, transportable and well-defined style templates',
-  question1: 'what are style sheets?',
-  question2: 'what is the advantage of collapsing white space?',
-  question3: 'what are applets?'
+  answer1:'Enables you to build consistent, transportable and well-defined style templates',
+  question1: ['[1]what are style sheets?' + '[2]What is the Advantage of Collapsing White Space' + '[3]What are Applets?']
+  
 },
  hfive: {
-  answer1:'allow you to put a scroolling text in a web pages',
-  question1: 'What is a marquee?',
-  question2: 'what are applets?',
-  question3: 'What is the use of alternative text in image mapping?'
+  answer1:'Allows you to put a scroolling text in a web pages',
+  question1: ['[1]What is a marquee?' + '[2]What are applets?' + '[3]What is the use of Alternative text in image mappping?']
+ },
+
+one: {
+  answer1:'First President of the United States',
+  question1: ['[1]Who is George Washington?'+ '[2]Who is John Madison?' + '[3]Who is Matama Ghandhi?']
+},
+two: {
+  answer1:'Budapest',
+  question1: ['[1]What is a Capital of Hungary'+'[2]What is a Capital of Nepal? '+ '[3]What is a Capital of France?']
+},
+three: {
+  answer1: 'Taxes',
+  question1:['[1]What is every citizen civil duty?' + '[2]What is a Credit Card?' + '[3]What is Monetary Balance?']
+},
+four: {
+  answer1: 'Mississippi River',
+  question1:[ '[1]River of USA' + '[2]River of Egypt' + '[3]River of Thailand']
+},
+five: {
+  answer1: 'Nepal type of Government',
+  question1: ['[1]Republic Democarcy' + '[2]Monarchy' + '[3]Totalitarianism'] 
 }
 }
 //const answers = [hone, htwo, hthree, hfour, hfive]
 
 
 $(() => {   
-
+  
 $('.boxes').on('click', (e) => {
 //console.log(e.currentTarget.id)
 let obj = e.currentTarget.id
